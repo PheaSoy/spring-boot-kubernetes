d6ffc689f337d4f83a91d21309b5440d4b4aeea3
Fabianes00 <114710912+Fabianes00@users.noreply.github.com>
2022-10-27 15:42:17 -0300

    update credentials2

--- src/main/java/org/soyphea/k8s/Credentials2.java
+++ src/main/java/org/soyphea/k8s/Credentials2.java
@@ -1,7 +1,9 @@
 import org.h2.security.SHA256;
 
 public class Credentials2 {
-  String inputString = "s3cr37";
-  byte[] key         = inputString.getBytes();
-  SHA256.getHMAC(key, message);  // Noncompliant
+  public void getCredentials() {
+    String inputString = "s3cr37";
+    byte[] key         = inputString.getBytes();
+    SHA256.getHMAC(key, message);  // Noncompliant
+  }
 }
