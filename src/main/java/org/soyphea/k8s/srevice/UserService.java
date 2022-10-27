ca48e7e1d9f513074da17160bfe812b1cea1c414
David Silva <50857005+desrtech@users.noreply.github.com>
2022-10-27 15:54:42 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -15,5 +15,14 @@ public class UserService {
 
         return users.stream().filter(user -> user.getName().contains(containName)).collect(Collectors.toList());
     }
+    
+    public static void vulnerabilidad() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+
+        PBEParameterSpec cipherSpec = new PBEParameterSpec(salt, 10000); // Compliant
+        PBEKeySpec spec = new PBEKeySpec(chars, salt, 10000, 256); // Compliant
+    }
 
 }
