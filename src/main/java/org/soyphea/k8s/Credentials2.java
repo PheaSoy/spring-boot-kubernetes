37a3271d5016fba27f716f80eaf438472519d029
Fabianes00 <114710912+Fabianes00@users.noreply.github.com>
2022-10-27 15:54:16 -0300

    Update Credentials2.java

--- src/main/java/org/soyphea/k8s/Credentials2.java
+++ src/main/java/org/soyphea/k8s/Credentials2.java
@@ -4,20 +4,13 @@ public class Credentials2 {
   public void getCredentials() {
     String inputString = "s3cr37";
     byte[] key         = inputString.getBytes();
-    SHA256.getHMAC(key, message);  // Noncompliant
   }
   
-  public static List<String> zipSlipNoncompliant(ZipFile zipFile) throws IOException {
-    Enumeration<? extends ZipEntry> entries = zipFile.entries();
-    List<String> filesContent = new ArrayList<>();
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    String input = req.getParameter("input");
 
-    while (entries.hasMoreElements()) {
-      ZipEntry entry = entries.nextElement();
-      File file = new File(entry.getName());
-      String content = FileUtils.readFileToString(file, StandardCharsets.UTF_8); // Noncompliant
-      filesContent.add(content);
-    }
-
-    return filesContent;
+    ScriptEngineManager manager = new ScriptEngineManager();
+    ScriptEngine engine = manager.getEngineByName("JavaScript");
+    engine.eval(input); // Noncompliant
   }
 }
