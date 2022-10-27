cb1f218e2579ab6990faf43b3e8c35592fcb6bfb
David Silva <50857005+desrtech@users.noreply.github.com>
2022-10-27 15:51:02 -0300

    Update Vulnerabilidad.java

--- src/main/java/org/soyphea/k8s/Vulnerabilidad.java
+++ src/main/java/org/soyphea/k8s/Vulnerabilidad.java
@@ -4,7 +4,6 @@ public class Vulnerabilidad {
     SecureRandom random = new SecureRandom();
     byte[] salt = new byte[16];
     random.nextBytes(salt);
-
     PBEParameterSpec cipherSpec = new PBEParameterSpec(salt, 10000); // Compliant
     PBEKeySpec spec = new PBEKeySpec(chars, salt, 10000, 256); // Compliant
   }
