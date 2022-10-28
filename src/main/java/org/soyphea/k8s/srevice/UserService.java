b6019a8a131e865becb322443c2b9ddec15d4238
David Silva <50857005+desrtech@users.noreply.github.com>
2022-10-27 22:59:45 -0300

    Update UserService.java

--- src/main/java/org/soyphea/k8s/srevice/UserService.java
+++ src/main/java/org/soyphea/k8s/srevice/UserService.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.security.SecureRandom;
+import javax.crypto.spec.PBEParameterSpec;
 
 @Service
 public class UserService {
