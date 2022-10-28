04eec61d07c70ecb8ff35b292f3d440c7d0bbe7e
Fabianes00 <114710912+Fabianes00@users.noreply.github.com>
2022-10-28 15:28:23 -0300

    Update Credentials2.java

--- src/main/java/org/soyphea/k8s/Credentials2.java
+++ src/main/java/org/soyphea/k8s/Credentials2.java
@@ -24,5 +24,7 @@ public class Credentials2 {
   public void run2(javax.servlet.http.HttpServletRequest request) throws ClassNotFoundException {
       String name = request.getParameter("name");
       Class clazz = Class.forName(name);  // Noncompliant
+    
+    
   }
 }
