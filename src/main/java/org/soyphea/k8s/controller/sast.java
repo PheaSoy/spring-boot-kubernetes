708a4197f26ecf9b8ac856f7b77561d433bbe845
rsanzana <40894875+rsanzana@users.noreply.github.com>
2022-10-29 20:21:45 -0300

    Delete sast.java

--- src/main/java/org/soyphea/k8s/controller/sast.java
+++
@@ -1,6 +0,0 @@
-public void run(javax.servlet.http.HttpServletRequest request) throws ClassNotFoundException {
-    String sast = request.getParameter("sast");
-    if (this.allowed.contains(sast)) {
-        Class clazz = Class.forName(sast);
-    }
-}
